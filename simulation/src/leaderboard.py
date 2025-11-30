7e63c1b75c59b06fd5024bbc6e9456259f5ab2c5
Calvin Tran <calvintran1478@protonmail.com>
2025-11-30 17:53:37 -0500

    Add position numbers to leaderboard

--- simulation/src/leaderboard.py
+++ simulation/src/leaderboard.py
@@ -8,8 +8,9 @@ class Leaderboard:
         self.batch = batch
 
         self.boxes = tuple(shapes.Rectangle(70, 555 - i * 28, 200, 26, color=driver.team_colour+(220,), batch=self.batch) for i, driver in enumerate(reversed(self.ordered_drivers)))
-        self.ts = tuple(text.Label(driver.name_acronym, x=75, y=560 - i * 28, batch=self.batch, font_size=14, color=(255, 255, 255)) for i, driver in enumerate(reversed(self.ordered_drivers)))
+        self.ps = tuple(text.Label(str(i+1), x=90, y=560 - i * 28, batch=self.batch, font_size=14, color=(255, 255, 255), anchor_x="center") for i, driver in enumerate(reversed(self.ordered_drivers)))
+        self.ts = tuple(text.Label(driver.name_acronym, x=120, y=560 - i * 28, batch=self.batch, font_size=14, color=(255, 255, 255)) for i, driver in enumerate(reversed(self.ordered_drivers)))
 
     def update(self) -> None:
         self.boxes = tuple(shapes.Rectangle(70, 555 - i * 28, 200, 26, color=driver.team_colour+(220,), batch=self.batch) for i, driver in enumerate(reversed(self.ordered_drivers)))
-        self.ts = tuple(text.Label(driver.name_acronym, x=75, y=560 - i * 28, batch=self.batch, font_size=14, color=(255, 255, 255)) for i, driver in enumerate(reversed(self.ordered_drivers)))
+        self.ts = tuple(text.Label(driver.name_acronym, x=120, y=560 - i * 28, batch=self.batch, font_size=14, color=(255, 255, 255)) for i, driver in enumerate(reversed(self.ordered_drivers)))
